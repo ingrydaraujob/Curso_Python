37fadd9089c724ed8c6a41d75882a34b92696e41
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-03-13 20:29:53 -0300

    011

---
+++ desafio 011.py
@@ -0,0 +1,5 @@
+largura = float(input("digite o valor da largura da parede em metros:"))
+altura = float(input("digite o valor da altura da parede em metros:"))
+area = largura + altura
+tinta = area / 2
+print(f"para pintar a parede de {area} metros de area, sera necessario {tinta} litros de tinta")
