b2c7f5e59aacfcad9328f91d56f5a7c7237b2c02
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-03-14 16:10:09 -0300

    018

---
+++ desafio 018.py
@@ -0,0 +1,8 @@
+import math
+angulo = float(input("Digite o valor do angulo:"))
+seno = math.sin(angulo)
+cosseno = math.cos(angulo)
+tangente = math.tan(angulo)
+print("O seno do angulo é:",seno)
+print("o cosseno é:", cosseno)
+print(" a tangente é:",tangente) 
