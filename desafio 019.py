4cb5b6c8b7970e28fcf0431f7ae94cb5aab15d17
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-03-14 17:50:03 -0300

    Update desafio 019.py

--- desafio 019.py
+++ desafio 019.py
@@ -1,7 +1,18 @@
 import random
+
+# Lista para armazenar os nomes dos alunos
 alunos = []
+
+# Loop para ler os nomes dos alunos
 for i in range(4):
-    nome = input("digite o nome do aluno{}:".format(i+1))
-    alunos.append(nome)
-escolhido = random.choice(alunos)
-print("o aluno escolhido para apagar o quadro é:",escolhido)
+    nome_aluno = input("Digite o nome do aluno {}: ".format(i+1))
+    alunos.append(nome_aluno)
+
+# Embaralhando a ordem dos alunos
+random.shuffle(alunos)
+
+# Exibindo a ordem sorteada de apresentação dos trabalhos
+print("Ordem sorteada de apresentação dos trabalhos:")
+for i, aluno in enumerate(alunos, start=1):
+    print(f"{i}. {aluno}")
+
