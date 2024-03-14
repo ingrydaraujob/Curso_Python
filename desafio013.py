37154500e354302683c3babf1597bfe90bccef8e
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-03-13 21:55:38 -0300

    013

---
+++ desafio013.py
@@ -0,0 +1,5 @@
+salario = float(input("digite o salario do funcionário:"))
+aumento = salario * 15/100
+novo_sal = salario + aumento
+print("o novo salario é de:",novo_sal)
+print("e o aumento foi de:",aumento)
