799b6f519f50dc9fc3785b6daf2b1ef33da93895
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-03-14 20:54:06 -0300

    017

--- desafio 017.py
+++ desafio 017.py
@@ -2,4 +2,4 @@ import math
 cateto_opo = float(input("digite o valor do cateto oposto:"))
 cateto_adja = float(input("digite o valor do cateto adjacente:"))
 hipotenusa = math.sqrt(cateto_opo **2 + cateto_adja ** 2 )
-print("O comprimento da hipotenusa é:", hipotenusa)
+print("O comprimento da hipotenusa é:{:.2f}".format (hipotenusa))
