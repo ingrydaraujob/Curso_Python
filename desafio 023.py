14b88f1bcbd29edcc7af2b6fc672b6839889254a
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-03-28 18:23:13 -0300

    023

---
+++ desafio 023.py
@@ -0,0 +1,6 @@
+cidade = str(input("digite o nome da cidade:"))
+
+if cidade.lower().startswith("santo"):
+    print("o nome da cidade começa com 'santo'.")
+else:
+    print("essa cidade não começa com 'santo'.")
