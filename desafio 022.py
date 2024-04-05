ee54b521efe5e06f498e8de5afe9c2baebb256a4
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-05 12:32:29 -0300

    022

--- desafio 022.py
+++ desafio 022.py
@@ -1,12 +1,8 @@
 num = int(input("dígite um numero com 4 dígitos:")) 
-
-if num < 1000 or num > 9999:
-    print("pfvr, digite um numero que tenha 4 dígitos")
-else:
-    milhar = num // 1000
-    centena = (num % 1000) // 100
-    dezena =  (num % 100) // 10
-    unidade = num % 10 
+unidade = num // 1 % 10
+dezena = num // 10 % 10
+centena = num // 100 % 10
+milhar = num // 1000 % 10  
 
 print("milhar",milhar)
 print("centena",centena)
