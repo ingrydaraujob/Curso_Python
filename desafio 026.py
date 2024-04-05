aaebcafd382d6e6ebe05f674f23a8933602f6e70
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-05 14:04:07 -0300

    026

--- desafio 026.py
+++ desafio 026.py
@@ -1,7 +1,7 @@
-nome = input("digite seu nome completo:")
-
-primeira_posição = min(nome.find())
-ultima_posição = max(nome.rfind())
+nome = input("digite seu nome completo:").strip()
+n = nome.split()
+primeira_posição = n[0]
+ultima_posição = n[len(n)-1]
 
 print(f"o primeiro nome: {primeira_posição}")
 print(f"o ultima nome: {ultima_posição}")
