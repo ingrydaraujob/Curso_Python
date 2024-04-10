4511f0a36168a5ca462dd97af37aeebaa870014a
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-10 20:43:18 -0300

    027

--- desafio 027.py
+++ desafio 027.py
@@ -1,1 +1,13 @@
- 
+from random import randint
+from time import sleep
+computador = randint(0, 5) #esse comando faz o computador sortear os numeros qur colocamos 
+print("-=- " * 20)
+print("vou pensar em um número entre 0 e 5. tente adivinhar...")
+print("-=- " * 20)
+jogador = int(input("em que numero eu pensei?"))
+print("processando...")
+sleep(2)
+if jogador == computador:
+    print("PARABÉNS!!, voce venceu")
+else:
+    print(f"GANHEI!!! eu pensei em {computador} e não em {jogador} ")
