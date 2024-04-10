76fd63957bd9aecfdd962fbb0957d2a541d522a2
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-10 19:57:54 -0300

    032

---
+++ desafio 032.py
@@ -0,0 +1,7 @@
+num1 = int(input("digite o primeiro numero:"))
+num2 = int(input("digite o segundo numero:"))
+num3 = int(input("digite o terceiro numero:"))
+maior = max(num1 , num2 ,num3)
+menor = min(num1 , num2 , num3)
+print("O maior numero é:",maior)
+print("O menor numero é:", menor)
