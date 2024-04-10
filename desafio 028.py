9c9a9f77f2f40c683e19991f6a12c7db3628db3e
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-10 20:48:28 -0300

    028

--- desafio 028.py
+++ desafio 028.py
@@ -1,7 +1,7 @@
 velocidade = int(input("digite a velocidade de do carro:"))
 
 
-if velocidade >= 80:
+if velocidade > 80:
     multa = (velocidade - 80) * 7
     print("voce foi multado")
     print("voce vai pagar uma multa de:", multa)
