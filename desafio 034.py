0a213956ebb0da2c0d0119b8e4436ee5fb85353d
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-10 23:30:56 -0300

    034

--- desafio 034.py
+++ desafio 034.py
@@ -2,9 +2,15 @@ reta1 = float(input("digite o comprimento da reta1: "))
 reta2 = float(input("digite o comprimento da reta2: "))
 reta3 =float(input("digite o comprimento da reta3: "))
 
-if reta1 == reta2 == reta3:
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
+    print("podem formar um triangulo")
+else:
+    ("não podem formar um triangulo")
+
+    
+'''if reta1 == reta2 == reta3:
     print("pode formar um triangulo equilátero")
 elif reta1 != reta2 != reta3:
     print("pode formar um triangulo escaleno")
 else:
-    print("triangulo isóceles")
+    print("triangulo isóceles")'''
