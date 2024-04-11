69d70a6e586efba1f85ad58300aa951ed6ce5b38
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-10 23:15:54 -0300

    031

--- desafio 031.py
+++ desafio 031.py
@@ -1,5 +1,7 @@
+#from datetime import date
 ano = int(input("digite um ano:"))
-
+#if ano == 0:
+    #ano= date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0 :
     print("o ano é bissexto")
 else:
