e61b92b393bee4cc88cc9cad2c4916b56d3d4f31
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-13 02:13:36 -0300

    038

--- desafio 038.py
+++ desafio 038.py
@@ -15,3 +15,17 @@ elif idade > 18:
     print(f"voce deveria ter se alistado há {saldo} anos")   
     ano = atual - saldo
     print("seu alistamento foi em:",ano)
+
+    '''ano = int(input("digite o ano do seu nascimento:"))
+
+if ano > 2006:
+    tempo = 2024 - ano 
+    tempo1 = 18 - tempo
+    print("voce ainda vai se alistar")
+    print(f"faltam {tempo1} ano(s) para o alistamento")
+elif ano == 2006:
+    print("é a hora de se alistar:")
+else:
+    tempo = 2024 - ano - 18
+    print("já passou do tempo de se alistar")
+    print("ja se passaram",tempo)'''
