bea469b500eee0c46346249b3bb327c9ddc6bf3e
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-13 01:38:51 -0300

    035

--- desafio 035.py
+++ desafio 035.py
@@ -1,5 +1,16 @@
 casa = float(input("digite o valor da casa:"))
 salario = float(input("digite o seu salario:"))
-anos = int(input("digite em quantos anos voce quer pagar a sua casa:"))
-
+anos = int(input("digite em quantos anos de financiamento:"))
+prestação = casa / (anos * 12)
+minimo = salario * 30/100
+print("a prestação vai ser de:" , prestação)
+print("minino:",minimo)
+if prestação <= minimo:
+    print("-=-"*6)
+    print("emprestimo pode ser CONCEDIDO")
+    print("-=-"*6)
+else:
+    print("-=-"*6)
+    print("emprestimo NEGADO")
+    print("-=-"*6)
  
