ddcf6e8bbd6a4c9afa0f7b18526c2a98fee86b04
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-24 16:34:56 -0300

    055

--- desafio 055.py
+++ desafio 055.py
@@ -1,6 +1,5 @@
 sexo = input("Digite o sexo (M/F): ").upper()
 while sexo not in ['M', 'F']:
     sexo = input("Valor incorreto! Digite apenas M para masculino ou F para feminino: ").upper()
-
 print("Sexo digitado corretamente:", sexo)
 
