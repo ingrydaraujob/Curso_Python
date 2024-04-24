45b15761e4a59cc0c20452480d4d989f5a588023
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-24 16:54:37 -0300

    56

---
+++ desafio 056.py
@@ -0,0 +1,22 @@
+from random import randint
+from time import sleep
+computador = randint(0, 10) #esse comando faz o computador sortear os numeros qur colocamos 
+print("-=- " * 13)
+print("vou pensar em um número entre 0 e 10. tente adivinhar...")
+print("-=- " * 13)
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input("em que numero eu pensei?"))
+    palpites += 1
+    print("processando...")
+    sleep(0.5)
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print("mais... tente mais uma vez")
+        elif jogador > computador:
+            print("menos... tente mais uma vez.")
+print(f"Acertou com {palpites} PARABÉNS!!, voce venceu")
+    
