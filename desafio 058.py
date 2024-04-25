63697558540e1b47c6f25f2ee7d1b31fec0617e9
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-25 00:47:00 -0300

    058

--- desafio 058.py
+++ desafio 058.py
@@ -1,4 +1,14 @@
 from math import factorial
 num = int(input("digite um numero para calcular o seu fatorial:"))
 fatorial = factorial(num)
-print(f"o fatorial de {num} é {fatorial}")
+print(f"o fatorial de {num} é {fatorial}") #simples
+
+"""num = int(input("digite um numero para calcular o seu fatorial:"))
+c = num
+f = 1
+while c > 0:
+    print(c , end='')
+    print("x" if c > 1 else "=",end="")
+    f *= c
+    c-=1  
+print(f)"""
