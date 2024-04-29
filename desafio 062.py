d0208902915338f6224a8fb6e90517225696a5c8
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-04-29 15:10:44 -0300

    062

---
+++ desafio 062.py
@@ -0,0 +1,8 @@
+num = cont = soma = 0
+while num not in[999]:
+    num = int(input("digite o um número inteiro:"))
+    soma += num
+    cont += 1
+print(f"voce digitou {cont-1} números e a soma entre eles foi de {soma-999}")
+print("número certo!")
+
