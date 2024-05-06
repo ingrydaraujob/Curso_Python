5be815434a19d612140eb3019f367f0e444fcb9f
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-05-06 19:01:10 -0300

    065

--- desafio 065.py
+++ desafio 065.py
@@ -1,11 +1,12 @@
 while True:
     numero = int(input("digite o valor que quer para a tabuada:"))
-    if numero < 0:
-        print("programa encerrado.")
-        break
     print("-=-"*10)
     print(f"Tabuada do {numero}:")
     for i in range(1, 11):
         resultado = numero * i
         print(f"{numero} x {i} = {resultado}")
-      
+    if numero < 0:
+        print("programa encerrado.")
+        break
+    print("-=-"*10)
+   
