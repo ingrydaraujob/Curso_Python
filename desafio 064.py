d7238b60157f13f7847dfc1d48b614908adaf003
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-05-06 15:51:53 -0300

    064

---
+++ desafio 064.py
@@ -0,0 +1,8 @@
+num = soma = cont = 0
+while True:
+    num = int(input("digite um numero inteiro:"))
+    if num == 999:
+        break
+    soma = soma + num
+    cont = cont + 1
+print(f"a soma dos numeros inteiros foi de {soma} e foram necessarios {cont} numeros para obter esse valor")
