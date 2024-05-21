2ea487b3ada81013047bcb4ffe03ce2bcce2476a
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-05-21 19:56:53 -0300

    077

--- desafio 077.py
+++ desafio 077.py
@@ -1,13 +1,13 @@
 numeros = []
 while True:
-    num = int(input("digite um numero [ou 000 para parar]:"))
-    if num == 000:
-        break
+    num = int(input("digite um numero:"))
     if num not in numeros:
         numeros.append(num)
         print("valores cadastrados com sucesso:")
     else:
         print("valor duplicado! nao será adicionado.")
-
+    res=str(input("quer continuar? [S/N]")).upper().strip()[0]
+    if res in "Nn":
+        break
 print("-=-"*17)
 print("voce digitou os numeros:",sorted(numeros))
