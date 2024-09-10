723b4905b7d4ed731473bc27968c4a8c10c9ce7b
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-09-09 22:49:47 -0300

    091

--- desafio 091.py
+++ desafio 091.py
@@ -1,6 +1,8 @@
 galera = list()
 pessoa = dict()
+soma = media = 0
 while True:
+    pessoa.clear()
     pessoa["nome"] = str(input("Nome:"))
     while True:
         pessoa["sexo"] = str(input("sexo[M/F]:")).upper()[0]
@@ -8,10 +10,29 @@ while True:
             break
         print("ERRO! pfvr, digite apenas M ou F")
     pessoa["idade"]= int(input("Idade:"))
+    soma += pessoa["idade"]
+    galera.append(pessoa.copy())
     while True:
         resp = str(input("quer continuar[S/N]:")).upper()[0]
         if resp in "SN":
             break
         print("ERRO! responda apenas S ou N ")
-    if resp == "S":
+    if resp == "N":
         break
+print("-="*20)
+print(f"ao todo teos {len(galera)} pessoas cadastradas")
+media = soma / len(galera)
+print(f"a media de idade é de {media:5.2f} anos")
+print("as mulheres cadastradas foram",end=" ")
+for p in galera:
+    if p["sexo"] in "Ff":
+        print(f"{p["nome"]} ", end="")
+print()
+print("lista das pessoas que estao acima da media:")
+for p in galera:
+    if p["idade"] >=media:
+        print("  ")
+        for k , v in p.items():
+            print(f"{k}, {v}", end=" ")
+            print()
+print("ENCERRADO")
