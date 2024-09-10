0de1334ebe05336caa476ea770dc17b8b319f901
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-09-10 20:40:06 -0300

    092

---
+++ desafio 092.py
@@ -0,0 +1,20 @@
+dict = {}
+list = []
+dict["jogador"] = str(input("Nome do jogador:"))
+partidas = int(input(f"quantas partidas {dict['jogador']} jogou?"))
+for c in range(0,partidas):
+    list.append(int(input(f"quantos gols na partida {c}?")))
+dict["gols"] = list[:]
+dict["total"] = sum(list)
+print("-="*20)
+print(dict)
+print("-="*20)
+for k, v in dict.items():
+    print(f"o campo {k} tem o valor {v}")
+print("-="*20)
+print(f"o jogador{dict['jogador']} jogou {len(dict['gols'])} partidas ")
+for i , v in enumerate(dict["gols"]):
+    print(f"na partida {i}, fez {v} gols")
+print(f"foi um total de {dict["total"]} gols")
+
+
