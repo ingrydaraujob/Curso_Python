5de65c75fa7f819f5d815be89f185997373d6c61
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-09-25 00:33:03 -0300

    Create desafio 098.py

---
+++ desafio 098.py
@@ -0,0 +1,16 @@
+import datetime
+def eleição():
+    ano = datetime.date.today().year
+    nas = ano - nascimento
+    if  nas < 16:
+        return (f" com {nas} anos: VOTO NEGADO.")
+    elif 16 <= nas < 18 or nas > 65:
+        return (f"com {nas} anos: VOTO OPCIONAL.")
+    else :
+        return (f"com {nas} anos: VOTO OBRIGATÓRIO.")
+    print(f"voce tem {nas} : ",end=" " )
+
+
+
+nascimento = int(input("Em que ano você nasceu?"))
+print(eleição())
